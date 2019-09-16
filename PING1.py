c22168a62709a39ec4a9edfe3089d84731ee20d2
D@rk_Devil#666 <54716053+Darkdevil730@users.noreply.github.com>
2019-09-16 18:49:26 +0700

    Add files via upload

---
+++ PING1.py
@@ -0,0 +1,32 @@
+#!/usr/python2
+
+##################
+##################
+##################
+###
+###
+###
+#################
+#################
+#################
+###
+###
+###
+###
+###
+###
+### FARREL PUTRA ANDHIKA ( D@rk_Devil#666 )
+
+#mengimpor
+import os
+
+#memulai ping jaringan
+os.system("ping 8.8.8.8")
+#membuat perulangan
+
+a = 0
+while( a < 3 ):
+    print("Ping berhasil")
+    
+#ping by Farrel Putra Andhika
+#Jangan Recode
